069210d78f9bbe5bbe7dd471d5a22712e6ac57d5
subpair <michael.reichel1@iu-study.org>
2022-12-30 08:34:36 +0100

    - fixed GitHub action

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ module "main_region_config" {
   domain_name              = var.domain_name
 }
 
-//module "sub_region_config" {
-//  source                   = "./infrastructure"
-//  region                   = var.sub_region
-//  cidr_v4_ssh_own_ip       = var.v4_own_ip
-//  cidr_v6_ssh_own_ip       = var.v6_own_ip
-//  aws_route53_zone_primary = aws_route53_zone.primary
-//  domain_name              = var.domain_name
-//}
+module "sub_region_config" {
+  source                   = "./infrastructure"
+  region                   = var.sub_region
+  cidr_v4_ssh_own_ip       = var.v4_own_ip
+  cidr_v6_ssh_own_ip       = var.v6_own_ip
+  aws_route53_zone_primary = aws_route53_zone.primary
+  domain_name              = var.domain_name
+}
 
 //module "third_region_config" {
 //  source                   = "./infrastructure"
